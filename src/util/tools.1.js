9828a004755a64d3332c474b2bd1d094ea5671f9
alicecoding <2542840096@qq.com>
2018-09-04 09:21:05 +0800

    vue.config

--- src/util/tools.1.js
+++ src/util/tools.1.js
@@ -3,34 +3,34 @@
  * @return []
  */
 exports.textActiveOff = (arrs, payload) => {
-    let arr = arrs;
-    const { index, isAll } = payload;
-    if (arr && arr.length) {
-        if (isAll !== undefined) {
-            arr[index].isActive = false;
-            arr[index].zIndex = arr[index].dragIndex;
-        } else {
-            arr[index].isActive = true;
-            arr[index].zIndex = 1000;
-        }
-        if (arr.length !== 1) {
-            arr.map((item, i) => {
-                if (i !== index && arr[i].isActive) {
-                    arr[i].isActive = false;
-                    arr[i].zIndex = arr[i].dragIndex;
-                }
-            });
-        }
+  const arr = arrs;
+  const { index, isAll } = payload;
+  if (arr && arr.length) {
+    if (isAll !== undefined) {
+      arr[index].isActive = false;
+      arr[index].zIndex = arr[index].dragIndex;
+    } else {
+      arr[index].isActive = true;
+      arr[index].zIndex = 1000;
     }
-    return arr;
+    if (arr.length !== 1) {
+      arr.map((item, i) => {
+        if (i !== index && arr[i].isActive) {
+          arr[i].isActive = false;
+          arr[i].zIndex = arr[i].dragIndex;
+        }
+      });
+    }
+  }
+  return arr;
 };
 
 exports.del_drag = (arr, index) => arr.filter((item, key) => key !== index);
 
 exports.now_time = () => {
-    const myDate = new Date();
-    const hours = myDate.getHours();
-    const minutes = myDate.getMinutes() < 10 ? `0${myDate.getMinutes()}` : myDate.getMinutes();
-    const am = hours > 12 ? ' AM' : ' PM';
-    return `${hours}:${minutes}${am}`;
+  const myDate = new Date();
+  const hours = myDate.getHours();
+  const minutes = myDate.getMinutes() < 10 ? `0${myDate.getMinutes()}` : myDate.getMinutes();
+  const am = hours > 12 ? ' AM' : ' PM';
+  return `${hours}:${minutes}${am}`;
 };
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    productionSourceMap: false,
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://47.94.138.247:9040',
-                changeOrigin: true,
-            },
-        },
+  productionSourceMap: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://47.94.138.247:9040',
+        changeOrigin: true,
+      },
     },
+  },
 };
