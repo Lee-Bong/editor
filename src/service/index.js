eb9b7c8cf089cfb4b822dcf1ddd791d803ff1390
alicecoding <2542840096@qq.com>
2018-09-21 09:51:52 +0800

    merge

--- src/service/index.js
+++ src/service/index.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'https://test-bfe.meiyou.com';
+// const { NODE_ENV } = process.env;
 
-const { NODE_ENV } = process.env;
-const api = NODE_ENV === 'development' ? 'https://test-bfe.meiyou.com' : 'https://bfe.meiyou.com';
+const api = window.location.host.indexOf('test-') === 0 ? 'https://test-bfe.meiyou.com' : 'https://bfe.meiyou.com';
 const getPageInfo = pageId => axios.get(`${api}/api/we/page`, { params: { page_id: pageId } })
   .then(property('data'));
 
