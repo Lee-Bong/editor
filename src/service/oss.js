af1e51104e7a84f3c68025dde03fa199d42d59db
alicecoding <2542840096@qq.com>
2018-09-27 10:53:21 +0800

    build

--- src/service/oss.js
+++ src/service/oss.js
@@ -1,6 +1,6 @@
 import axios from './request';
 
-const api = process.env.NODE_ENV === 'development' || window.location.host.indexOf('test-') !== 0 ? 'https://test-bfe.meiyou.com' : 'https://bfe.meiyou.com';
+const api = process.env.NODE_ENV === 'development' || window.location.host.indexOf('test-') === 0 ? 'https://test-bfe.meiyou.com' : 'https://bfe.meiyou.com';
 const UP_API = `${api}/api/oss-up`;
 
 export default async function upload(file) {
