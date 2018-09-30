4ef0ae11f20474763fb7169a8ab969b45976aacf
戴振捷 <daizj@mingyuanyun.com>
2018-09-30 11:47:57 +0800

    update: 优化分页构建

--- vue.config.js
+++ vue.config.js
@@ -28,14 +28,53 @@ const config = {
       entry: 'src/pages/admin/main.js',
       template: 'public/admin.html',
       filename: 'admin.html',
-      chunks: ['chunk-vendors', 'chunk-common', 'admin'],
+      chunks: ['chunk-vendors', 'chunk-admin-vendors', 'chunk-common', 'admin'],
     },
     view: {
       entry: 'src/pages/view/main.js',
       template: 'public/view.html',
       filename: 'view.html',
-      chunks: ['chunk-vendors', 'chunk-common', 'view'],
+      chunks: ['chunk-vendors', 'chunk-view-vendors', 'chunk-common', 'view'],
     },
+  },
+
+  chainWebpack: (cfg) => {
+    const options = module.exports;
+    const { pages } = options;
+    const pageKeys = Object.keys(pages);
+
+    // Long-term caching
+
+    const IS_VENDOR = /[\\/]node_modules[\\/]/;
+
+    cfg.optimization
+      .splitChunks({
+        cacheGroups: {
+          vendors: {
+            name: 'chunk-vendors',
+            priority: -10,
+            chunks: 'initial',
+            minChunks: 2,
+            test: IS_VENDOR,
+            enforce: true,
+          },
+          ...pageKeys.map(key => ({
+            name: `chunk-${key}-vendors`,
+            priority: -11,
+            chunks: chunk => chunk.name === key,
+            test: IS_VENDOR,
+            enforce: true,
+          })),
+          common: {
+            name: 'chunk-common',
+            priority: -20,
+            chunks: 'initial',
+            minChunks: 2,
+            reuseExistingChunk: true,
+            enforce: true,
+          },
+        },
+      });
   },
 };
 
